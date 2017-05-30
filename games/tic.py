5af9612a8717c1a7832cdd3c3e0a95e01d3fd8cc
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2017-05-30 00:15:19 -0500

    working on moves in tic tac toe

--- games/tic.py
+++ games/tic.py
@@ -1,24 +1,60 @@
 
-userone = ''
-def startingPiece():
+def letter():
     userone = input('User enter X or O: ').upper()
     if userone not in ('X', 'O'):
-        print('Enter the right damn letter!')
-        startingPiece()
+        print('Enter an X or an O, por favor')
+        letter()
     else:
         print('User 1 entered ' + userone)
+        return userone
 
-startingPiece()
+userone = letter()
 
-if 
+usertwo = ''
+if userone == 'X':
+    usertwo = 'O'
+else:
+    usertwo = 'X'
+
+print()
+print('user one is: ' + userone)
+print('user two is: ' + usertwo)
+print()
 
 board = []
 
 for x in range(0, 3):
-    board.append('[]' * 3)
+    board.append('[ ]' * 3)
 
 def makeboard(list):
-    for row in list:
-        print(' '.join(row))
+#    for colNum, colBox in enumerate(list, start = 1):
+#        print(str(colNum) + '  ' + str(colNum))
+    print ('    1     2     3')
+    for rowNum, rowBox in enumerate(list, start = 1):
+        print(str(rowNum) + ' ' + ' '.join(rowBox))
+        print()
 
 makeboard(board)
+
+print(len(board))
+
+def taketurn(turn):
+    print('IT IS ' + turn + '\'S TURN...')
+    print('PICK YOUR COORDINATES, ' + turn)
+    row = input('ROW: ')
+    #if row
+    col = input('COLUMN: ')
+
+turn = userone
+taketurn(turn)
+
+
+
+
+
+
+#      _   _
+#     | | | |
+#     | |_| |
+#      \__, |
+#      |___/
