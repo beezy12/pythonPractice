9a6d689d4cbe6f5bc299b28fb72960830637ceae
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2017-06-06 23:41:03 -0500

    cleaned up for Python3

--- redditBots/dogone/dog.py
+++ redditBots/dogone/dog.py
@@ -1,26 +1,43 @@
 import praw
-import config
 import time
 import os
 
 
-def botLogin():
-    print('logging in....')
-    reddit = praw.Reddit(username = config.username,
-                password = config.password,
-                client_id = config.client_id,
-                client_secret = config.client_secret,
-                user_agent = 'beezyTester:dog comment responder:v0.1')
-    #print('logged in as ' + reddit.user.me())
+def authenticate():
+    print('authenticating....')
+    # did have all the credentials below when I had a config, but changed to praw.ini and just have to pass in the name of the ini
+    reddit = praw.Reddit('dogbot', user_agent = 'beezyTester:dog comment responder:v0.1')
+    print('authenticated as: {}'.format(reddit.user.me()))
     return reddit
 
 
+def get_saved_comments():
+    # this means if the file doesn't exist, make a new list
+    if not os.path.isfile('comments_replied_to.txt'):
+        comments_replied_to = []
+    else:
+    # the 'r' here means 'read'
+        with open('comments_replied_to.txt', 'r') as file:
+            comments_replied_to = file.read()
+	    # the split here adds each comment as a new element in the list
+            comments_replied_to = comments_replied_to.split('\n')
+            comments_replied_to = list(filter(None, comments_replied_to))
+
+    return comments_replied_to
+
+
+def main():
+    while True:
+        reddit = authenticate()
+        comments_replied_to = get_saved_comments()
+        print(comments_replied_to)
+        runBot(reddit, comments_replied_to)
+
 
 def runBot(reddit, comments_replied_to):
-    print('getting 25 comments')
-
-
-    for comment in reddit.subreddit('test').comments(limit=25):
+    limit = 12
+    print('getting ' + str(limit) + ' comments')
+    for comment in reddit.subreddit('test').comments(limit = limit):
         if "dog" in comment.body and comment.id not in comments_replied_to and not comment.author == reddit.user.me():
             print('Found a doggy')
             comment.reply('I like degs too')
@@ -36,30 +53,12 @@ def runBot(reddit, comments_replied_to):
 
 
 
-def get_saved_comments():
-    # this means if the file doesn't exist, make a new list
-    if not os.path.isfile('comments_replied_to.txt'):
-        comments_replied_to = []
-    else:
-    # the 'r' here means 'read'
-    	with open('comments_replied_to.txt', 'r') as file:
-            comments_replied_to = file.read()
-	    # the split here adds each comment as a new element in the list
-            comments_replied_to = comment_replied_to.split('\n')
 
-    return comments_replied_to
+print('prior to import: {}'.format(__name__))
+
+if __name__ == '__main__':
+    main()
 
 
-
-reddit = botLogin()
-
-#print(reddit)
-
-# moved this: comments_replied_to = []
-comments_replied_to = get_saved_comments()
-print(comments_replied_to)
-
-while True:
-    runBot(reddit, comments_replied_to)
 
 
