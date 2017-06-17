a599ee570df324933f8e25560ebf7a88b845e6cf
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2017-06-16 23:42:02 -0500

    added new items

--- auto/projects/fantasy.py
+++ auto/projects/fantasy.py
@@ -1,4 +1,6 @@
 # this program tallies up all items in players inventory along with the count of items
+# it then adds the dragonsloot, which is a list of strings, to it
+# so this is adding a list of items to a dictionary of items
 
 stuff = {
     'rope': 1, 
@@ -8,6 +10,8 @@ stuff = {
     'arrow': 12
 }
 
+dragonsloot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
 
 def countInventory(inventory):
     print()
@@ -15,7 +19,6 @@ def countInventory(inventory):
 
     count = 0
     for k, v in inventory.items():
-        print(v)
         print(v, k)
         count = count + v
 
@@ -23,6 +26,24 @@ def countInventory(inventory):
     print('total: ' + str(count))
     print()
 
-countInventory(stuff)
+    return count
+
+
+def addToInventory(inventory, dragons):
+    for item in range(len(dragons)):
+        if dragons[item] in inventory.keys():
+            inventory[dragons[item]] += 1
+        else:
+            inventory.setdefault(dragons[item], 1)
+
+    return inventory
+
+
+
+
+inv = addToInventory(stuff, dragonsloot)
+
+countInventory(inv)
+
 
     
